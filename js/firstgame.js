14fe2c91f39fe35c519fc84df7bdeb17242bf8ec
GoGoCav <gogocav@gmail.com>
2014-09-27 15:24:51 +0800

    auto scale to full screen

--- js/firstgame.js
+++ js/firstgame.js
@@ -6,6 +6,10 @@ game.States.boot = function() {
 		game.load.image('loading', 'assets/preloader.gif');
 	};
 	this.create = function() {
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.scale.pageAlignHorizontally = true;
+		this.scale.pageAlignVertically = true;
+		this.scale.setScreenSize(true);
 		game.state.start('preload');
 	};
 };
